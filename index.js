08154563f3e54e56c6b6a6f7ce0de6bdbe1f67e1
Mohib <mohibullahmohim2020@gmail.com>
2024-06-11 13:35:07 +0600

    vercel deploy and product pass to database successfully

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const stripe = require('stripe')(process.env.STRIPE_SK);
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SK);
 const app = express();
-const port = process.env.PORT || 9000 ;
+const port = process.env.PORT || 9000;
 
-// middle-wares 
+// middle-wares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // mongo db connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -19,65 +19,76 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
+// server link : https://asset-management-server-brown.vercel.app/
 async function run() {
   try {
-
     // Connect the client to the server	(optional starting in v4.7)
-    const database = client.db('corporateManage');
-    const usersCollection = database.collection('users')
-    
-    // employee or manager All user ------------------------------------------------
-    app.get('/users', async(req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result)
-    })
+    const database = client.db("corporateManage");
+    const usersCollection = database.collection("users");
+    const productCollection = database.collection("products");
 
-    // employee or manager get from database ------------------------------------------------
-    app.get('/users/:email', async(req, res) => {
-      const email = req.params.email ;
-      const query = {email : email}
+    //---------------------------------------- READ ALL DATA -----------------------------------
+    // employee or manager All user ------------------------------------------------------------
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //----------------------------------- READ SPECIFIC DATA -----------------------------------
+    // employee or manager get from database ---------------------------------------------------
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
       const result = await usersCollection.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    //------------------------------------------- POST DATA ------------------------------------
+    // employee or manager post in database ----------------------------------------------------
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // payment api -----------------------------------------------------------------------------
-    app.post('/payment-intent', async(req,res)=> {
-      const {price} = req.body;
-      console.log(price,'price', typeof price);
-      if(price === 0){
+    app.post("/payment-intent", async (req, res) => {
+      const { price } = req.body;
+      console.log(price, "price", typeof price);
+      if (price === 0) {
         return;
       }
-      const amount = parseInt( price * 100 );
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
-        amount : amount,
-        currency : 'usd',
-        payment_method_types : ['card']
-
-      });          
-      res.send({clientSecret: paymentIntent.client_secret})
-    })
-
-    // employee or manager post in database -------------------------------------------------
-    app.post('/users', async (req, res) => {
-      const user = req.body ;
-      const result = await usersCollection.insertOne(user);
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    
+    // Product api ----------------------------------------------------------------------------
+    app.post('/products', async (req, res) => {
+      const product = req.body ;
+      const result = await productCollection.insertOne(product) ;
       res.send(result)
     })
 
     // Send a ping to confirm a successful connection
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
 // server call
-app.get('/', (req, res) => {
-    res.send('Corporate Management Server is running BROH');
-})
-app.listen(port, ()=>{
-    console.log(`Corporate Management Server Port is ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("Corporate Management Server is running BROH");
+});
+app.listen(port, () => {
+  console.log(`Corporate Management Server Port is ${port}`);
+});
