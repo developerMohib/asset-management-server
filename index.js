23adf1b25cf09288318da72869609de683886f01
Mohib <mohibullahmohim2020@gmail.com>
2024-06-14 03:04:19 +0600

    my requested assests delete api okay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SK);
 const app = express();
 const port = process.env.PORT || 9000;
@@ -28,7 +28,7 @@ async function run() {
     const database = client.db("corporateManage");
     const usersCollection = database.collection("users");
     const productCollection = database.collection("products");
-    const requProductCollec = database.collection('requ-product')
+    const requProductCollec = database.collection("requ-product");
 
     //------------------------------------------- POST DATA ------------------------------------
     // employee or manager All user ------------------------------------------------------------
@@ -66,16 +66,30 @@ async function run() {
     });
 
     // ------------------------------------- Requested Product ---------------------------------
-    app.get('/requ-product',async(req, res)=>{      
-      const result = await requProductCollec.find().toArray() ;
-      res.send(result)
-    })
-    
-    app.post('/requ-product',async(req, res)=>{
-      const item = req.body ;
-      const result = await requProductCollec.insertOne(item) ;
-      res.send(result)
-    })
+    app.get("/requ-product", async (req, res) => {
+      const result = await requProductCollec.find().toArray();
+      res.send(result);
+    });
+    app.get("/requ-product/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log("delete id", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await requProductCollec.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/requ-product", async (req, res) => {
+      const item = req.body;
+      const result = await requProductCollec.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/requ-product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await requProductCollec.deleteOne(query);
+      res.send(result);
+    });
 
     //----------------------------------------- PAYMENT DATA -----------------------------------
     //-------------------------------------- payment api ---------------------------------------
